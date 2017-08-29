636c9a7874b21c1b1e48f87b8fbedbf4389545b0
Abubakar Saleh Usman <abubakarsalehusman@gmail.com>
2017-08-29 19:42:22 +0300

    Add files via upload

---
+++ break.py
@@ -0,0 +1,2 @@
+import webbrowser;
+webbrowser.open("https://www.youtube.com/watch?v=P7OUkjXU0Es");
---
+++ breaks.py
@@ -0,0 +1,9 @@
+import webbrowser
+import time
+hutu = 0
+
+print("this program start at "+time.ctime())
+while(hutu < 3):
+	time.sleep(10)
+	webbrowser.open("https://www.youtube.com/watch?v=dA0L2Fb10Zo&list=PLFIZ5myVkC0GNBIoa_uxfkq-ZPB5RYhtw&index=2")
+	count = count + 1
